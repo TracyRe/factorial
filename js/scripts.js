76680b519f3dfe368dae14a1953ff3a264d067a7
Tracy Reith <treith@treith.com>
2019-01-23 22:39:38 -0800

    Add code to separate user input into an array of numbers, multiply all numbers in array, return result, minimally style page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
+
   $("#factorial").submit(function(event){
     event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+
+    var output = [];
+    for (var i = userInput; i > 1 ; i = --userInput ) {
+      output.push(i);
+    }
+    console.log(output);
+
+    var result = 1;
+    multiands = output;
+    multiands.forEach(function(multiand) {
+      result *= multiand;
+    });
+
+    $(".result").text(result);
+
+
+    });
+    $("button#redo").click(function(){
+    $("input#userInput").val("");
+    $(".result").text("");
   });
 });
